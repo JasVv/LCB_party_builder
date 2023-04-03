fcda331caf6bfd179366eb3561d0dcf2ab40d4f1
JasVv <32508327+JasVv@users.noreply.github.com>
2023-04-03 17:15:47 +0900

    基礎部分

---
+++ lcb-party-builder/src/MainApp.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue'
+
+const character = ref([])
+const skill = ref([])
+
+let data = ref({
+  character: [],
+  skill: [],
+})
+
+function recalculate() {
+  data.value = {
+    character: character,
+    skill: skill,
+  }
+}
+</script>
+
+<template>
+  <div>
+  	<input type="checkbox" v-model="character" value="1" v-on:change="recalculate()" />1
+    <input type="checkbox" v-model="character" value="2" v-on:change="recalculate()" />2
+    <input type="checkbox" v-model="character" value="3" v-on:change="recalculate()" />3
+  </div>
+  
+  <div>
+  	<input type="checkbox" v-model="skill" value="4" v-on:change="recalculate()" />4
+    <input type="checkbox" v-model="skill" value="5" v-on:change="recalculate()" />5
+    <input type="checkbox" v-model="skill" value="6" v-on:change="recalculate()" />6
+  </div>
+  
+  <label>{{ data }}</label>
+</template>
