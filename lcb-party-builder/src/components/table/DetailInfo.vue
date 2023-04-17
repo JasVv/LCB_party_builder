5b4d089fa5d56234ad608042508fe99ac90dce83
seigi yamazaki <yamazaki.seigi@donuts.ne.jp>
2023-04-17 18:16:21 +0900

    スキル詳細欄仮

--- lcb-party-builder/src/components/table/DetailInfo.vue
+++ lcb-party-builder/src/components/table/DetailInfo.vue
@@ -14,27 +14,24 @@ defineProps<{
         <span class="ml-3">スキル詳細</span>
       </summary>
       <div class="ml-3">
-        {{ v.personality.skill[0].name }}
-        <label td :class="selectSinColorCss(v.personality.skill[0].sin)">
-          {{ 
-            `${v.personality.skill[0].physics}${v.personality.skill[0].base_attack + (v.personality.skill[0].coin_attack * v.personality.skill[0].coin_number)} 
-              (${v.personality.skill[0].base_attack}, 
-                ${v.personality.skill[0].coin_attack >= 0 ? `+${v.personality.skill[0].coin_attack}` : v.personality.skill[0].coin_attack} * ${v.personality.skill[0].coin_number})` 
-          }}
-        </label>
-        <label>
-        {{ 
-            `
-            ${ v.personality.skill[0].text } \n
-            B \n
-            C
-            ` 
-        }}
-        
-        </label>
-        <label v-for="(coin_effect, idx) in v.personality.skill[0].coin_effect" :key="idx">
-        {{ coin_effect.number }} {{ coin_effect.text }}
-        </label>
+        <dl>
+          <template v-for="(skill, i) in v.personality.skill" :key="i">
+            <dt class="mt-2">
+              {{ skill.name }} 
+              <span :class="selectSinColorCss(skill.sin)" class="ml-3">
+                {{ 
+                  `${skill.physics}${skill.base_attack + (skill.coin_attack * skill.coin_number)} 
+                    (${skill.base_attack}, 
+                      ${skill.coin_attack >= 0 ? `+${skill.coin_attack}` : skill.coin_attack} * ${skill.coin_number})` 
+                }}
+              </span>
+            </dt>
+            <dd class="ml-3">{{ skill.text }}</dd>
+            <dd v-for="(coin_effect, idx) in skill.coin_effect" :key="idx" class="whitespace-pre-wrap ml-3">
+              {{ coin_effect.number }} {{ coin_effect.text }}
+            </dd>
+          </template>
+        </dl>
       </div>
     </details>
   </td>
